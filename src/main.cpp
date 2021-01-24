999794a931c1e909dda9adadaf4533fcb783f854
DeepDarkDweller <cnc33@students.uwf.edu>
2021-01-24 03:21:11 -0600

    Updated file with additional line for added happiness and cheer.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 #include<string>
+#include<stdio.h>
 
 using namespace std;
 
 int main(){
     cout << "HELLO FUNGUS!" << endl;
+    printf("Nice to meet you <3\n");
     return 0;
 }
